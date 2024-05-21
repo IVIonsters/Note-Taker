056cd5395689737b40bd9c85a81792f76e33756f
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-21 17:36:39 -0500

    noteroute.js updates
    defined variables in noteroute file
    
    installed uuidv4 to generate ID's with random numbers for code.
    
    pseduo code added for functions needed

---
+++ routes/noteroute.js
@@ -0,0 +1,16 @@
+//UUIDV4 Used to generate unique id for each note
+const { uuid } = require('uuidv4');
+const fs = require('fs');
+const router = require('express').Router();
+
+//GET request to get all notes
+
+
+//Post request to add a new note
+
+
+//Delete request to delete a note
+
+
+//Export the router
+module.exports = router;
