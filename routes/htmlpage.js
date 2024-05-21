0a1a521772165f042c344aa57f0922d8c65793c6
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-20 23:29:51 -0500

    Update htmlpage.js

---
+++ routes/htmlpage.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const path = require('path');
+
+// GET route for homepage
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// GET route for notes page
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+
+// Export
+module.exports = router;
