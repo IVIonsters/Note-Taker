8ab25e84e6c0567ca0dd988cdfa7ce727ea0a85b
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-21 23:05:59 -0500

    noteroute.js changes
    simplified error to err for better syntax
    
    restructured order of variables and definitions to debugged error.
    
    removed duplicate res.json(notes); which was preventing system from starting correctly.
    
    changed uuidv4 ID application
    
    changed return to return correct version of notes

--- routes/noteroute.js
+++ routes/noteroute.js
@@ -1,14 +1,15 @@
 //UUIDV4 Used to generate unique id for each note
-const { uuid } = require('uuidv4');
-const fs = require('fs');
 const router = require('express').Router();
+const fs = require('fs');
+const {v4: uuidv4 } = require('uuid');
+
 
 //GET request to get all notes
 router.get('/notes', (req, res) => {
     //Read the db.json file
-    fs.readFile('./db/db.json', (error, data) => {
-        if (error) {
-            console.error(error);
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            console.error(err);
             return res.status(500).json({ message: 'Error reading notes, Server Error' });
         }
         //Parse the data from the file
@@ -16,16 +17,14 @@ router.get('/notes', (req, res) => {
         //Return the notes as json
         res.json(notes);
     });
-    //Return the notes as json
-    res.json(notes);
 });
 
 //Post request to add a new note
 router.post('/notes', (req, res) => {
     //Read the db.json file
-    fs.readFile('./db/db.json', (error, data) => {
-        if (error) {
-            console.error(error);
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            console.error(err);
             return res.status(500).json({ message: 'Error reading notes, Server Error' });
         }
         //Parse the data from the file
@@ -33,18 +32,18 @@ router.post('/notes', (req, res) => {
         //Create a new note object
         const newNotes = {
             ... req.body,
-            id: uuid(),
+            id: uuidv4(),
         };
         //Add the new note to the notes array
         notes.push(newNotes);
         //Write the updated notes array back to the file
-        fs.writeFile('./db/db.json', JSON.stringify(notes), error => {
-            if (error) {
-                console.error(error);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+            if (err) {
+                console.error(err);
                 return res.status(500).json({ message: 'Error writing notes, Server Error' });
             }
             //Return the new note as json
-            res.json(newNote);
+            return res.json(notes);
         });
     });
 });
@@ -52,19 +51,19 @@ router.post('/notes', (req, res) => {
 //Delete request to delete a note
 router.delete('/notes/:id', (req, res) => {
     //Read the db.json file
-    fs.readFile('./db/db.json', (error, data) => {
-        if (error) {
-            console.error(error);
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            console.error(err);
             return res.status(500).json({ message: 'Error reading notes, Server Error' });
         }
         //Parse the data from the file
         const notes = JSON.parse(data);
         //Filter out the note to delete
-        const newNotesFiltered = notes.filter(note => note.id !== req.params.id);
+        const newNotesFiltered = notes.filter(note => note.id != req.params.id);
         //Write the updated notes array back to the file
-        fs.writeFile('./db/db.json', JSON.stringify(newNotesFiltered), error => {
-            if (error) {
-                console.error(error);
+        fs.writeFile('./db/db.json', JSON.stringify(newNotesFiltered), err => {
+            if (err) {
+                console.error(err);
                 return res.status(500).json({ message: 'Error writing notes, Server Error' });
             }
             //Return a message that the note was deleted
