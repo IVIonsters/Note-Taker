5c50fb26b0c1e3ba39232254de26760414b6f831
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-21 20:17:41 -0500

    noteroute changes
    added delete for notes and route.
    
    restructured pathways and deleted DEVELOP Folder

--- routes/noteroute.js
+++ routes/noteroute.js
@@ -6,7 +6,7 @@ const router = require('express').Router();
 //GET request to get all notes
 router.get('/notes', (req, res) => {
     //Read the db.json file
-    fs.readFile('./develop/db/db.json', (error, data) => {
+    fs.readFile('./db/db.json', (error, data) => {
         if (error) {
             console.error(error);
             return res.status(500).json({ message: 'Error reading notes, Server Error' });
@@ -23,7 +23,7 @@ router.get('/notes', (req, res) => {
 //Post request to add a new note
 router.post('/notes', (req, res) => {
     //Read the db.json file
-    fs.readFile('./develop/db/db.json', (error, data) => {
+    fs.readFile('./db/db.json', (error, data) => {
         if (error) {
             console.error(error);
             return res.status(500).json({ message: 'Error reading notes, Server Error' });
@@ -33,12 +33,12 @@ router.post('/notes', (req, res) => {
         //Create a new note object
         const newNotes = {
             ... req.body,
-            id: uuidv4(),
+            id: uuid(),
         };
         //Add the new note to the notes array
         notes.push(newNotes);
         //Write the updated notes array back to the file
-        fs.writeFile('./develop/db/db.json', JSON.stringify(notes), error => {
+        fs.writeFile('./db/db.json', JSON.stringify(notes), error => {
             if (error) {
                 console.error(error);
                 return res.status(500).json({ message: 'Error writing notes, Server Error' });
@@ -50,7 +50,28 @@ router.post('/notes', (req, res) => {
 });
 
 //Delete request to delete a note
-
+router.delete('/notes/:id', (req, res) => {
+    //Read the db.json file
+    fs.readFile('./db/db.json', (error, data) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Error reading notes, Server Error' });
+        }
+        //Parse the data from the file
+        const notes = JSON.parse(data);
+        //Filter out the note to delete
+        const newNotesFiltered = notes.filter(note => note.id !== req.params.id);
+        //Write the updated notes array back to the file
+        fs.writeFile('./db/db.json', JSON.stringify(newNotesFiltered), error => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ message: 'Error writing notes, Server Error' });
+            }
+            //Return a message that the note was deleted
+            res.json({ message: 'Note deleted' });
+        });
+    });
+});
 
 //Export the router
 module.exports = router;
