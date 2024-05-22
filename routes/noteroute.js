1aaf0adaf2a6fa5428ba6305626e3120c9da5e80
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-21 19:43:38 -0500

    Update noteroute.js
    added GET request to retrieve notes from db.json
    
    added if statement to catch errors
    
    added POST request to add new note and parse data.
    
    added if statement to catch errors and log

--- routes/noteroute.js
+++ routes/noteroute.js
@@ -4,10 +4,50 @@ const fs = require('fs');
 const router = require('express').Router();
 
 //GET request to get all notes
-
+router.get('/notes', (req, res) => {
+    //Read the db.json file
+    fs.readFile('./develop/db/db.json', (error, data) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Error reading notes, Server Error' });
+        }
+        //Parse the data from the file
+        const notes = JSON.parse(data);
+        //Return the notes as json
+        res.json(notes);
+    });
+    //Return the notes as json
+    res.json(notes);
+});
 
 //Post request to add a new note
-
+router.post('/notes', (req, res) => {
+    //Read the db.json file
+    fs.readFile('./develop/db/db.json', (error, data) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Error reading notes, Server Error' });
+        }
+        //Parse the data from the file
+        const notes = JSON.parse(data);
+        //Create a new note object
+        const newNotes = {
+            ... req.body,
+            id: uuidv4(),
+        };
+        //Add the new note to the notes array
+        notes.push(newNotes);
+        //Write the updated notes array back to the file
+        fs.writeFile('./develop/db/db.json', JSON.stringify(notes), error => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ message: 'Error writing notes, Server Error' });
+            }
+            //Return the new note as json
+            res.json(newNote);
+        });
+    });
+});
 
 //Delete request to delete a note
 
